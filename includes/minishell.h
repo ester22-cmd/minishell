06dec51e8274b6026e6397b53ff97264f6880d9e
Amanda Ferreira Ramos Almeida <amaferre@c3r5p4.42sp.org.br>
2025-01-27 18:42:47 -0300

    atualização do .h

--- includes/minishell.h
+++ includes/minishell.h
@@ -10,24 +10,24 @@
 # include <sys/wait.h>
 
 // Estruturas básicas
-typedef struct s_token {
-    char            *value;
-    int             type;
-    struct s_token  *next;
+typedef struct	s_token {
+	char	*value;
+	int		type;
+	struct s_token *next;
 }   t_token;
 
 typedef struct s_command {
-    char            **args;
-    char            *input_file;
-    char            *output_file;
-    int             append_mode;
-    struct s_command *next;
-}   t_command;
+	char	**args;
+	char	*input_file;
+	char	*output_file;
+	int		append_mode;
+	struct s_command *next;
+} t_command;
 
 // Protótipos das funções principais
-void    setup_signals(void);
-t_token *tokenize(char *line);
-t_command *parse_commands(t_token *tokens);
-int     execute_pipeline(t_command *cmd);
+void		setup_signals(void);
+t_token		*tokenize(char *line);
+t_command	*parse_commands(t_token *tokens);
+int			execute_pipeline(t_command *cmd);
 
 #endif
