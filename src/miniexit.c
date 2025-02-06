313da1c786d8f4122b8dae689073461c42beda68
Amanda Ferreira Ramos Almeida <almeidafr.amanda@gmail.com>
2025-02-06 13:16:53 -0300

    atualização do projeto VIII

--- src/miniexit.c
+++ src/miniexit.c
@@ -2,23 +2,23 @@
 
 void	miniexit(t_mini *mini, t_node *node, t_list *list)
 {
-	int	code;
-	int	i;
+	int	code; // Variável para armazenar o código de saída do programa
+	int	i; // Variável para contar os caracteres na string do argumento
 
-	code = 0;
-	i = 0;
-	while (node->str[i])
-		i++;
-	if (i > 2)
-		printf("exit: too many arguments\n");
-	else
+	code = 0; // Inicializa o código de saída com o valor 0 (sucesso)
+	i = 0; // Inicializa o contador de caracteres como 0
+	while (node->str[i])  // Laço para contar o número de caracteres do primeiro argumento (node->str[1])
+		i++; // Incrementa o contador de caracteres até o final da string
+	if (i > 2) // Verifica se o número de caracteres no argumento é maior que 2
+		printf("exit: too many arguments\n"); // Se sim, imprime a mensagem de erro
+	else // Caso contrário, lida com o código de saída
 	{
-		if (node->str[1] && ft_strcmp(node->str[1], "exit"))
-			code = ft_atoi(node->str[1]);
-		free_reset(mini, list);
-		free_em_all(mini);
-		close(mini->st_out);
+		if (node->str[1] && ft_strcmp(node->str[1], "exit")) // Verifica se existe um segundo argumento e se ele não é "exit"
+			code = ft_atoi(node->str[1]); // Converte o segundo argumento para um inteiro e armazena em 'code'
+		free_reset(mini, list); // Função para liberar recursos (não detalhada no código fornecido)
+		free_em_all(mini); // Libera todos os recursos associados ao 'mini'
+		close(mini->st_out); // Fecha os descritores de arquivo relacionados a saída e entrada padrão
 		close(mini->st_in);
-		exit(code);
+		exit(code); //Finaliza o programa com o código de saída especificado
 	}
 }
--- src/minipwd.c
+++ src/minipwd.c
@@ -2,9 +2,10 @@
 
 void	minipwd(void)
 {
-	char	wd[1024];
+	char	wd[1024]; // Cria um vetor de caracteres 'wd' com capacidade para armazenar até 1024 caracteres.
 
-	getcwd(wd, sizeof(wd));
-	printf("%s\n", wd);
-	g_return = 0;
+	getcwd(wd, sizeof(wd)); // Obtém o diretório de trabalho atual e armazena no vetor 'wd'. A função getcwd preenche 'wd' com o caminho absoluto do diretório de trabalho.
+	printf("%s\n", wd); // Imprime o caminho do diretório de trabalho atual (armazenado em 'wd').
+	g_return = 0; 
+    g_return = 0;  // Define o valor de 'g_return' como 0 (provavelmente um indicador global de status de execução, definindo sucesso).
 }
